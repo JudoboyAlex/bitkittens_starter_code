770d1623f8134555b5cdbb8aa82be97b9f80d005
Alexander G. Yang <spark8402@rogers.com>
2019-06-20 17:05:05 -0400

    assignment done

--- bitkittens/static/js/cats.js
+++ bitkittens/static/js/cats.js
@@ -1,2 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
+
+let kittyButton = document.querySelector('.summon-cats');
+kittyButton.addEventListener('click', e => {
+    console.log('kitty');
+axios
+    .get("http://bitkittens.herokuapp.com/cats.json", {headers: {'X-Requested-With': 'XMLHttpRequest'}})
+    .then(response => {
+        console.log(response.data);
+        
+        const kittyName = document.querySelectorAll(".cat-box")
+        
+        kittyName.forEach( (box, i) => {  
+
+        const kittyImg = document.createElement("img");
+        box.innerHTML = '';
+        kittyImg.src = response.data.cats[i].photo;
+        kittyImg.alt = `Photo of ${response.data.cats[i].name}`; 
+        box.appendChild(kittyImg)
+    })
+  })
+})
+
 });
+
+
+
+
